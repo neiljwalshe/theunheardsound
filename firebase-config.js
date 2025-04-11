bb95abdc68611dffb9b5c0d189ae3b6a972e6830
neiljwalshe <neiljwalshe@googlemail.com>
2025-04-11 22:59:30 +1000

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebase configuration goes here, ensure you've replaced this with your actual Firebase project info.
+const firebaseConfig = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "YOUR_AUTH_DOMAIN",
+    databaseURL: "YOUR_DATABASE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
